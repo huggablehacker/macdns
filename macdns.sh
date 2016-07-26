6829ba56bba9f6892ce416b08304a31bb7baf127
Huggable Hacker <huggablehacker@users.noreply.github.com>
2016-07-26 18:56:48 -0400

    Update macdns.sh

--- macdns.sh
+++ macdns.sh
@@ -8,11 +8,5 @@ fi
 
 dscacheutil -q host -a name $1
 
-#Props to Shrop:
-#http://mark.shropshires.net/blog/mac-os-x-tip-look-host-ip-addresses-hosts-etchosts?utm_content=buffer27f14&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer
-
-# Most DNS tools (like dig and nslookup) perform IP lookups against DNS servers, 
-# ignoring /etc/hosts entries. Luckily, OS X provides the dscacheutil command to perfom a 
-# number of functions including checking local host entries. Below is an example that I
-# used to verify the hostname/IP address combo that my system had registered in 
-# /etc/hosts.
+# Props to Shrop:
+# http://mark.shropshires.net/blog/mac-os-x-tip-look-host-ip-addresses-hosts-etchosts
